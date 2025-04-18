b2c903583cbe6acf7c97eafce045544a12b4b9f2
Ailton Gualdez de Oliveira <ailton.gualdez979@gmail.com>
2025-04-18 12:23:12 -0300

    12:22-18/04/25

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Conte de ${c} áte...`)
+    c++
+} while (c <= 10)
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Conte de ${c} áte...`)
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
