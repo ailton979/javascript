1b0f5e6ddbc6daefd6022c86c1c9b445b43c3c6a
Ailton Gualdez de Oliveira <ailton.gualdez979@gmail.com>
2025-04-19 14:02:45 -0300

    14:02-19/04/25

---
+++ aula16/anbiente.js
@@ -0,0 +1,17 @@
+/*
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par bb!'
+    } else {
+        return 'Ímpar bb!'
+    }
+}
+let res = parimpar(2596)
+console.log(`este numero e ${res}`)
+*/
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+console.log(soma(2, 5))
+
