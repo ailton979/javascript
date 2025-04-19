f1cb3ed3497e4c10f88f41ca7aa1db2608478f8b
Ailton Gualdez de Oliveira <ailton.gualdez979@gmail.com>
2025-04-19 10:06:07 -0300

    10:05-19/04/25

---
+++ aula15/ambiente.js
@@ -0,0 +1,25 @@
+/*
+let num = [2, 40, 100, 30, 5, 70, 90]
+
+num.push = 13
+
+num.sort(function(a, b){return a-b})
+
+
+for (pos = 0; pos<num.length; pos++) {
+    console.log(`No indice, a possição ${pos} tem o valor ${num[pos]} Que diz: Conte de ${num[pos]} até...100.`)
+    
+}
+*/
+
+
+var num = ["Ailton", "Adriana", "Afonso", "Silvana", "Igor", "Mayara", "Joel"]
+
+num.sort()
+
+
+for (pos = 0; pos<num.length; pos++) {
+    console.log(`No indice, a possição ${pos} tem o valor ${num[pos]} Que diz: Conte de ${num[pos]} até...100.`)
+    
+} 
+
